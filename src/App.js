c376484b90928cf90d723cd24a1f7273b5dceb9a
michael24b <michael_baaklini@hotmail.com>
2022-10-31 23:42:15 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <Fragment>
-      <Hero />;
+      <Hero />
       <Snippets />
       <Features />
       <Access />
